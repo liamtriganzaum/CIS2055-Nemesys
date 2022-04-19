5e315fe20881b657fc5e40e5419c0ac5302d0469
liamtriganzaum <liam.triganza.15@um.edu.mt>
2022-04-19 11:33:16 +0200

    Startup

---
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CIS2055___Nemesys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

4e502d2b0236b04768a9daf3aefd9ac4ed9f06bb
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2022-04-29 12:27:41 +0200

    Inheritance from Doucment removed (Investigation.cs)

--- CIS2055 - Nemesys/CIS2055 - Nemesys/Models/Documents/Investigation.cs
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Models/Documents/Investigation.cs
@@ -4,21 +4,38 @@ using System.Collections.Generic;
 
 namespace CIS2055___Nemesys.Models.Documents
 {
-    public class Investigation : Document
+    public class Investigation
     {
+        // todo put these constants outisde of the class scope 
+        public const int RAND_MIN = 0;
+        public const int RAND_MAX = 10;
 
+        private string IRN { get; set; } // investigation reference number
         private string DateOfAction { get; set; }
 
         private string Desc { get; set; }
         private string Investigator_Email { get; set; }
         private Status _status;
 
+        public static List<string> General_IRN;
+
+        public Random Random;
+
         public Investigation()
         {
             Random = new Random();
             // generate IRN
-            Generate_RN();
+            Generate_IRN();
         }
-        
+
+
+        private void Generate_IRN()
+        {
+            int RandomNumber = Random.Next(RAND_MIN, RAND_MAX);
+            while (General_IRN.Contains(RandomNumber.ToString()))
+            {
+                RandomNumber = Random.Next(RAND_MIN, RAND_MAX);
+            }
+        }
     }
 }

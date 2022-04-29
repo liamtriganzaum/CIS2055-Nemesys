919d4cb37e889b5b3d2071bd93e860ac9d2291ba
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2022-04-29 12:14:50 +0200

    Report Repository added

---
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Models/Repositories/ReportsRepository.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using CIS2055___Nemesys.Models.Documents;
+
+namespace CIS2055___Nemesys.Models.Repositories
+{
+    public class ReportsRepository
+    {
+        private List<Report> _reports;
+
+        public ReportsRepository()
+        {
+            if (_reports == null)
+            {
+                InitializeReports();
+            }
+        }
+
+        private void InitializeReports()
+        {
+            _reports = new List<Report>()
+            {
+                new Report(location:"Malta",
+                           h_type: HazardType.UnsafeAct, 
+                           description:"desctiption: ok",
+                           email:"email@mail.com",
+                           status:Status.Open),
+                new Report(location:"Malta",
+                    h_type: HazardType.UnsafeAct, 
+                    description:"desctiption: ok",
+                    email:"email@mail.com",
+                    status:Status.Open),
+                new Report(location:"Malta",
+                    h_type: HazardType.UnsafeAct, 
+                    description:"desctiption: ok",
+                    email:"email@mail.com",
+                    status:Status.Open)
+            };
+        }
+        
+        // todo this method has to return IEnumerable<Report> 
+        public IEnumerable<Document> GetAllDocuments()
+        {
+            return _reports;
+        }
+
+        public Document GetDocumentByRN(int DocumentRN)
+        {
+            return null;
+        }
+    }
+}

cfa3dbfcb8459451268898f5b8936530aa21915a
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2022-05-11 13:07:40 +0200

    `Create`, `Edit` and `Expand` Views have been added

---
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Views/Reports/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIS2055___Nemesys.Views.Reports
+{
+    public class Edit : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}

c58a5a7b40115d678c9e0cfc11b5db4314741f9c
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2022-05-11 13:02:09 +0200

    `Expand`, `Edit` get and post ActionResults added (ReportsController.cs)

--- CIS2055 - Nemesys/CIS2055 - Nemesys/Controllers/ReportsController.cs
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Controllers/ReportsController.cs
@@ -76,6 +76,74 @@ namespace CIS2055___Nemesys.Controllers
             
             return View();
         }
+        
+        public IActionResult Expand(int rrn)
+        {
+            var current_report = _reportsRepository.GetDocumentByRN(rrn);
+
+            if (current_report == null) return NotFound();
+
+            var Model = new ReportViewModel()
+            {
+                RRN                = current_report.RRN,
+                Title              = current_report.Title,
+                Location           = current_report.Location,
+                Desc               = current_report.Desc,
+                _hazardType        = current_report._hazardType,
+                DateAndTimeOfReport= current_report.DateAndTimeOfReport,
+                _status            = current_report._status
+            };
+
+            return View(Model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int rrn)
+        {
+            var report = _reportsRepository.GetDocumentByRN(rrn);
+            if (report != null)
+            {
+                var Model = new EditReportViewModel()
+                {
+                    Title = report.Title,
+                    Location = report.Location,
+                    _hazardType = report._hazardType,
+                    Desc = report.Desc,
+                    Reporter_Email = report.Reporter_Email,
+                    _status = report._status
+                };
+                
+                return View(Model);
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int rrn, [Bind("rrn, Title, Location, _hazardType, Desc, ReporterEmail, status")] EditReportViewModel ReportToUpdate)
+        {
+            var report = _reportsRepository.GetDocumentByRN(rrn);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                report.Title = ReportToUpdate.Title;
+                report.Desc = ReportToUpdate.Desc;
+                report.Location = ReportToUpdate.Location;
+                report._hazardType = ReportToUpdate._hazardType;
+                report._status = ReportToUpdate._status;
+                report.Reporter_Email = ReportToUpdate.Reporter_Email;
+
+                _reportsRepository.UpdateReport(report);
+                return RedirectToAction("Index");
+            }
+            
+            // todo not finished part (from bloggy)
+            return RedirectToAction("Index");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

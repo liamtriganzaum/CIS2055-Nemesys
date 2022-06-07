87e279e1452a699124fe815fccc7bffcc3cb19e9
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2022-06-07 12:44:46 +0200

    Configuration files updated

--- CIS2055 - Nemesys/CIS2055 - Nemesys/Views/Reports/Expand.cshtml.cs
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Views/Reports/Expand.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CIS2055___Nemesys.Views.Reports
 {
-    public class Expand : PageModel
+    public partial class ExpandReport : PageModel
     {
         public void OnGet()
         {

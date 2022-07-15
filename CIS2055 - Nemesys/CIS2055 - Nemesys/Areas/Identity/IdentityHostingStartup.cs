c1826d3c6609b402778b15a124c4a6cc0399cb59
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2022-07-15 11:29:20 +0200

    Final commit

---
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using CIS2055___Nemesys.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CIS2055___Nemesys.Areas.Identity.IdentityHostingStartup))]
+namespace CIS2055___Nemesys.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ CIS2055 - Nemesys/CIS2055 - Nemesys/Migrations/20220509122953_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CIS2055___Nemesys.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    RRN = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateAndTimeOfReport = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Desc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Reporter_Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    upvotes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.RRN);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reports",
+                columns: new[] { "RRN", "DateAndTimeOfReport", "Desc", "Location", "Reporter_Email", "Title", "upvotes" },
+                values: new object[] { 1, new DateTime(2022, 5, 9, 14, 29, 53, 214, DateTimeKind.Local).AddTicks(8518), "desctiption: hazardous", "Malta", "email@mail.com", "Disaster1", 0 });
+
+            migrationBuilder.InsertData(
+                table: "Reports",
+                columns: new[] { "RRN", "DateAndTimeOfReport", "Desc", "Location", "Reporter_Email", "Title", "upvotes" },
+                values: new object[] { 2, new DateTime(2022, 5, 9, 14, 29, 53, 219, DateTimeKind.Local).AddTicks(7426), "desctiption: hazardous", "Malta", "email@mail.com", "Disaster2", 0 });
+
+            migrationBuilder.InsertData(
+                table: "Reports",
+                columns: new[] { "RRN", "DateAndTimeOfReport", "Desc", "Location", "Reporter_Email", "Title", "upvotes" },
+                values: new object[] { 3, new DateTime(2022, 5, 9, 14, 29, 53, 219, DateTimeKind.Local).AddTicks(7483), "desctiption: hazardous", "Malta", "email@mail.com", "Disaster3", 0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}
